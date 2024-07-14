79c76d8b7b8161924b352fa8fb5b709b0ef488fa
Shubhraneel Gupta <btech10583.22@bitmesra.ac.in>
2024-07-14 22:57:39 +0530

    feature assets and simplification

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -10,84 +10,47 @@ import image7 from '../../assets/7.png';
 import './Features.scss';
 
 const Features = () => {
-  const scrollContainerRef = useRef(null);
-  const screenContainerRef = useRef(null);
-
-  useEffect(() => {
-    const handleWheel = (event) => {
-      if (scrollContainerRef.current) {
-        scrollContainerRef.current.scrollLeft += event.deltaY;
-      }
-    };
-
-    const scrollContainer = scrollContainerRef.current;
-    if (scrollContainer) {
-      scrollContainer.addEventListener('wheel', handleWheel);
-    }
-
-    return () => {
-      if (scrollContainer) {
-        scrollContainer.removeEventListener('wheel', handleWheel);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    const handleWheel = (event) => {
-      if (screenContainerRef.current) {
-        screenContainerRef.current.scrollLeft += event.deltaY;
-      }
-    };
-
-    const screenContainer = screenContainerRef.current;
-    if (screenContainer) {
-      screenContainer.addEventListener('wheel', handleWheel);
-    }
-
-    return () => {
-      if (screenContainer) {
-        screenContainer.removeEventListener('wheel', handleWheel);
-      }
-    };
-  }, []);
-
   return (
-    <>
-      <div className="heading">
-        What's in store for you.
-      </div>
-      <div className="subHeading">
-        Discover our features
-      </div>
-      <div ref={scrollContainerRef} className="scrollingContainer">
-        <div className="element first">
-          <div className="imageHeading">Verified Community</div>
-          <img src={image1} alt="Feature 1" />
+    <div className='wrapper'>
+      <div className="title">
+        <div className="heading">
+          What's in store for you.
         </div>
-        <div className="element">
-          <div className="imageHeading">Online Store</div>
-          <img src={image2} alt="Feature 2" />
-        </div>
-        <div className="element">
-          <div className="imageHeading">Mockup Engine</div>
-          <img src={image3} alt="Feature 3" />
+        <div className="subHeading">
+          Discover our features
         </div>
       </div>
-      <div ref={screenContainerRef} className="scrollingContainer">
-        <div className="element first">
-          <div className="imageHeading">Mockup-Engine Gen1</div>
-          <img src={image5} alt="Screen 1" />
-        </div>
-        <div className="element">
-          <div className="imageHeading">Rookus-Xpress Engine</div>
-          <img src={image6} alt="Screen 2" />
-        </div>
-        <div className="element">
-          <div className="imageHeading">Rookus Inpainting</div>
-          <img src={image7} alt="Screen 3" />
+        <div className="features-container">
+          <div className="row">
+          <div className="element">
+            <div className="imageHeading">Verified Community</div>
+            <img src={image1} alt="Feature 1" />
+          </div>
+          <div className="element">
+            <div className="imageHeading">Online Store</div>
+            <img src={image2} alt="Feature 2" />
+          </div>
+          <div className="element">
+            <div className="imageHeading">Mockup Engine</div>
+            <img src={image3} alt="Feature 3" />
+          </div>
+          </div>
+          <div className="row">
+            <div className="square">
+              <div className="imageHeading">Mockup-Engine Gen1</div>
+              <img src={image5} alt="Screen 1" />
+            </div>
+            <div className="square">
+              <div className="imageHeading">Rookus-Xpress Engine</div>
+              <img src={image6} alt="Screen 2" />
+            </div>
+            <div className="square">
+              <div className="imageHeading">Rookus Inpainting</div>
+              <img src={image7} alt="Screen 3" />
+            </div>
+          </div>
         </div>
       </div>
-    </>
   );
 };
 
